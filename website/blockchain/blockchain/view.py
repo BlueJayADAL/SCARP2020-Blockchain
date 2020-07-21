b19cf41ab8d41d443b6159034ce6ccbc14e398ce
Grace Cuff <gracecuff435@gmail.com>
2020-07-20 23:42:30 -0400

    update data display

--- website/blockchain/blockchain/view.py
+++ website/blockchain/blockchain/view.py
@@ -17,7 +17,21 @@ def homepage(request):
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
-        all_studies = xmlread()
+        tree = ET.parse("C:\\Users\\grace\\Blockchain Repository\\SCARP2020-Blockchain\\website\\blockchain\\blockchain\\static\\dataset\\SearchResults.xml")
+
+        all_studies = []
+
+        for search_results_xml in tree.iter("search_results"):
+            for study_xml in search_results_xml.iter("study"):
+                study_json = {}
+
+                for data in study_xml:
+                    if not data.text:
+                        study_json[data.tag] = data.text
+                    else:
+                        study_json[data.tag] = data.text.strip()
+
+                all_studies.append(study_json)
         return render(request=request, template_name="homepage.html", context={'all_studies': all_studies})
     else:
         # Return invalid login error message
@@ -34,20 +48,3 @@ def login_request(request):
     form = AuthenticationForm()
     return render(request=request, template_name="login.html", context={"form": form})
 
-
-def xmlread():
-    tree = ET.parse("C:\\Users\\grace\\Blockchain Repository\\SCARP2020-Blockchain\\website\\blockchain"
-                    "\\SearchResults.xml")
-
-    all_studies = []
-
-    for search_results_xml in tree.iter("search_results"):
-        for study_xml in search_results_xml("study"):
-            study_json = {}
-
-            for data in study_xml:
-                study_json[data.tag] = data.text.strip()
-
-            all_studies.append(study_json)
-    return all_studies
-    # return render(request, "homepage.html", {'all_studies': all_studies})
