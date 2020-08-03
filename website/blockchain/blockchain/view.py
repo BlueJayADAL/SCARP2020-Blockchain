b3beadbcf171ade6754ccf675028e05abfdc550b
Grace Cuff <gracecuff435@gmail.com>
2020-08-03 15:14:37 -0400

    attempt to implement role based access control

--- website/blockchain/blockchain/view.py
+++ website/blockchain/blockchain/view.py
@@ -1,3 +1,5 @@
+import xml
+
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
@@ -7,9 +9,39 @@ import xml.etree.ElementTree as ET
 from django.shortcuts import render
 from .forms import Profile_Form
 from .models import User_Profile
+from easyrbac import Role, User, AccessControlList
+
+FILE_TYPES = ['txt', 'xml']
 
 
-IMAGE_FILE_TYPES = ['png', 'jpg', 'jpeg']
+everyone_role = Role('everyone')
+admin_role = Role('admin')
+
+everyone_user = User(roles=[everyone_role])
+admin_user = User(roles=[admin_role, everyone_role])
+
+acl = AccessControlList()
+
+acl.resource_read_rule(everyone_role, 'GET', '/api/v1/employee/1/info')
+acl.resource_delete_rule(admin_role, 'DELETE', '/api/v1/employee/1/')
+
+# checking READ operation on resource for user `everyone_user`
+for user_role in [role.get_name() for role in everyone_user.get_roles()]:
+    assert acl.is_read_allowed(user_role, 'GET', '/api/v1/employee/1/info') == True
+
+# checking WRITE operation on resource for user `everyone_user`
+# Since you have not defined the rule for the particular, it will disallow any such operation by default.
+for user_role in [role.get_name() for role in everyone_user.get_roles()]:
+    assert acl.is_write_allowed(user_role, 'WRITE', '/api/v1/employee/1/info') == False
+
+# checking WRITE operation on resource for user `admin_user`
+for user_role in [role.get_name() for role in everyone_user.get_roles()]:
+    if user_role == 'admin':  # as a user can have more than one role assigned to them
+        assert acl.is_delete_allowed(user_role, 'DELETE', '/api/v1/employee/1/') == True
+    else:
+        assert acl.is_delete_allowed(user_role, 'DELETE', '/api/v1/employee/1/') == False
+
+
 def create_profile(request):
     form = Profile_Form()
     if request.method == 'POST':
@@ -19,7 +51,7 @@ def create_profile(request):
             user_pr.display_picture = request.FILES['display_picture']
             file_type = user_pr.display_picture.url.split('.')[-1]
             file_type = file_type.lower()
-            if file_type not in IMAGE_FILE_TYPES:
+            if file_type not in FILE_TYPES:
                 return render(request, 'profile_maker/error.html')
             user_pr.save()
             return render(request, 'profile_maker/details.html', {'user_pr': user_pr})
